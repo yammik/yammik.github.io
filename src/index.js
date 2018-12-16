d08e8f1b0a5a45599707f0a5a5a39c14922ee63c
yammik <mkim0514@gmail.com>
2018-12-15 21:20:26 -0500

    link click shows corresponding content

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
+  $('.content div').each(function(i) {
+    $(this).hide(0);
+  })
   $("#logo").fadeOut(0, function() {
     setTimeout(function () {
       $("#logo").fadeIn(1000);
     }, 0);
   });
-  
+
   $(".menu").each(function (i) {
     $(this).fadeOut(0, () => setTimeout(() => $(this).fadeIn(Math.random()*1500), 0))
+  })
+
+  $(".menuItem").on('click', function(e) {
+    const component = e.target.id.replace('Link','');
+    $('.content div').each(function(i) {
+      $(this).hide(0);
+    })
+    $(`#${component}`).show();
   })
 })
