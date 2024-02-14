42f3f2631f3e4aadc25ec287c2edd35eb326a299
yammik <23164118+yammik@users.noreply.github.com>
2024-02-14 03:24:37 -0500

    more fix

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"bsmoCQOfjwuKeaB9zHvs3ZdPJP5/cWkb0LOapwDXiw4=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"qSNcOkrfVWf6BYwbmek6LP54/lPkc8wpJTses4irhhY=\"\n}"
