e08eeb88a6940def4fa829b64616162cb72d2614
yammik <23164118+yammik@users.noreply.github.com>
2024-02-14 03:48:24 -0500

    sigh

--- next.config.mjs
+++ next.config.mjs
@@ -1,28 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  /**
-   * Enable static exports for the App Router.
-   *
-   * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
-   */
-  output: "export",
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
 
-  /**
-   * Set base path. This is the slug of your GitHub repository.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
-   */
-  basePath: "/yammik.github.io",
+let assetPrefix = "";
+let basePath = "/";
 
-  /**
-   * Disable server-based image optimization. Next.js does not support
-   * dynamic features with static exports.
-   *
-   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
-   */
+if (isGithubActions) {
+  // trim off `<owner>/`
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
-    unoptimized: true
+    loader: "imgix",
+    path: 'the "domain" of your Imigix source'
   }
 };
-
-export default nextConfig;
